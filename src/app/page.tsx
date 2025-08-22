af5977e13988ee6d5feb89cfdce7c78104c5e809
forsifall <vana.datlov.984@gmail.com>
2025-08-23 01:52:35 +0300

    добавил первую анимацию к диктафону, реализовал секцию вокруг него

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import Dictaphone from "@/components/dictaphone/dictaphone";
+import Dictaphone from "@/components/dictaphone/Dictaphone";
 import "./page.scss";
 
 export default function Home() {
   return (
     <section className="main-section">
-      <Dictaphone />
+      <div className="dictaphone-panel">
+        <Dictaphone />
+      </div>
     </section>
   );
 }
--- src/components/dictaphone/dictaphone.tsx
+++ src/components/dictaphone/dictaphone.tsx
@@ -1,10 +1,34 @@
-import "./dictaphone.scss"
-import Image from "next/image"
+"use client";
+
+import React, { useState } from "react";
+import "./dictaphone.scss";
+import Image from "next/image";
+
+enum Animation {
+  Up = "dictaphone-animation--up",
+  Down = "dictaphone-animation--down",
+}
+
+let animationIs:boolean = false;
 
 export default function Dictaphone() {
+  const [animation, setAnimation] = useState<Animation | null>(null);
+
+  const handlerClick = () => {
+    if(animationIs) return;
+    setAnimation(prev =>
+      prev === Animation.Up ? Animation.Down : Animation.Up
+    );
+  };
+
   return (
-    <div className="dictaphone-section">
-        <Image alt="microphone" src="/microphone.svg" width={50} height={50} />
+    <div
+      onClick={handlerClick}
+      onAnimationStart={() => animationIs = true}
+      onAnimationEnd={() => animationIs = false}
+      className={`dictaphone-section ${animation}`}
+    >
+      <Image alt="microphone" src="/microphone.svg" width={50} height={50} />
     </div>
-  )
+  );
 }
